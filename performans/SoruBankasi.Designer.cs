0aa5428436ba1a5399dfa91bce11f5b30e5208b8
Semih Noir <bssemih1434@outlook.com>
2023-01-01 16:19:43 +0300

    01.01.2023 düzenleme

---
+++ performans/SoruBankasi.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace performans
+{
+    partial class SoruBankasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SoruBankasi));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonSoruTemizle = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.btnTemizleCevaplar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxcevaplar = new System.Windows.Forms.RichTextBox();
+            this.btnSoruEkle = new System.Windows.Forms.Button();
+            this.richTextBoxSoru = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panelSorular = new System.Windows.Forms.Panel();
+            this.labelSoru = new System.Windows.Forms.Label();
+            this.richTextBoxCsoru = new System.Windows.Forms.RichTextBox();
+            this.buttonGeri = new System.Windows.Forms.Button();
+            this.buttonileri = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBoxsorucevaplari = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.cikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkinda = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panelSorular.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabControl1.Location = new System.Drawing.Point(-1, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(490, 292);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.buttonSoruTemizle);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.btnOlustur);
+            this.tabPage1.Controls.Add(this.btnTemizleCevaplar);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.btnSoruEkle);
+            this.tabPage1.Controls.Add(this.richTextBoxSoru);
+            this.tabPage1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(482, 264);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Soru Ekle";
+            // 
+            // buttonSoruTemizle
+            // 
+            this.buttonSoruTemizle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSoruTemizle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSoruTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonSoruTemizle.Location = new System.Drawing.Point(130, 193);
+            this.buttonSoruTemizle.Name = "buttonSoruTemizle";
+            this.buttonSoruTemizle.Size = new System.Drawing.Size(106, 31);
+            this.buttonSoruTemizle.TabIndex = 23;
+            this.buttonSoruTemizle.Text = "Soruyu Temizle";
+            this.buttonSoruTemizle.UseVisualStyleBackColor = true;
+            this.buttonSoruTemizle.Click += new System.EventHandler(this.buttonSoruTemizle_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 15);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Soru Metni";
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOlustur.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOlustur.Location = new System.Drawing.Point(38, 193);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(86, 31);
+            this.btnOlustur.TabIndex = 21;
+            this.btnOlustur.Text = "Test Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // btnTemizleCevaplar
+            // 
+            this.btnTemizleCevaplar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTemizleCevaplar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnTemizleCevaplar.Location = new System.Drawing.Point(256, 217);
+            this.btnTemizleCevaplar.Name = "btnTemizleCevaplar";
+            this.btnTemizleCevaplar.Size = new System.Drawing.Size(114, 28);
+            this.btnTemizleCevaplar.TabIndex = 20;
+            this.btnTemizleCevaplar.Text = "Cevapları Temizle";
+            this.btnTemizleCevaplar.UseVisualStyleBackColor = true;
+            this.btnTemizleCevaplar.Click += new System.EventHandler(this.btnTemizleCevaplar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBoxcevaplar);
+            this.groupBox1.Location = new System.Drawing.Point(256, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(191, 182);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cevaplar";
+            // 
+            // richTextBoxcevaplar
+            // 
+            this.richTextBoxcevaplar.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBoxcevaplar.Location = new System.Drawing.Point(5, 28);
+            this.richTextBoxcevaplar.Name = "richTextBoxcevaplar";
+            this.richTextBoxcevaplar.Size = new System.Drawing.Size(181, 148);
+            this.richTextBoxcevaplar.TabIndex = 9;
+            this.richTextBoxcevaplar.Text = "A) \nB) \nC) \nD) \nE) ";
+            // 
+            // btnSoruEkle
+            // 
+            this.btnSoruEkle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSoruEkle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSoruEkle.Location = new System.Drawing.Point(375, 217);
+            this.btnSoruEkle.Name = "btnSoruEkle";
+            this.btnSoruEkle.Size = new System.Drawing.Size(72, 28);
+            this.btnSoruEkle.TabIndex = 17;
+            this.btnSoruEkle.Text = "Soru Ekle";
+            this.btnSoruEkle.UseVisualStyleBackColor = true;
+            this.btnSoruEkle.Click += new System.EventHandler(this.btnSoruEkle_Click);
+            // 
+            // richTextBoxSoru
+            // 
+            this.richTextBoxSoru.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBoxSoru.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxSoru.Location = new System.Drawing.Point(38, 57);
+            this.richTextBoxSoru.Name = "richTextBoxSoru";
+            this.richTextBoxSoru.Size = new System.Drawing.Size(197, 130);
+            this.richTextBoxSoru.TabIndex = 16;
+            this.richTextBoxSoru.Text = "";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.panelSorular);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(482, 264);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Sorular";
+            // 
+            // panelSorular
+            // 
+            this.panelSorular.Controls.Add(this.labelSoru);
+            this.panelSorular.Controls.Add(this.richTextBoxCsoru);
+            this.panelSorular.Controls.Add(this.buttonGeri);
+            this.panelSorular.Controls.Add(this.buttonileri);
+            this.panelSorular.Controls.Add(this.groupBox2);
+            this.panelSorular.Location = new System.Drawing.Point(3, 3);
+            this.panelSorular.Name = "panelSorular";
+            this.panelSorular.Size = new System.Drawing.Size(478, 262);
+            this.panelSorular.TabIndex = 0;
+            this.panelSorular.Visible = false;
+            // 
+            // labelSoru
+            // 
+            this.labelSoru.AutoSize = true;
+            this.labelSoru.Location = new System.Drawing.Point(36, 35);
+            this.labelSoru.Name = "labelSoru";
+            this.labelSoru.Size = new System.Drawing.Size(32, 15);
+            this.labelSoru.TabIndex = 34;
+            this.labelSoru.Text = "Soru";
+            // 
+            // richTextBoxCsoru
+            // 
+            this.richTextBoxCsoru.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBoxCsoru.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBoxCsoru.Location = new System.Drawing.Point(38, 53);
+            this.richTextBoxCsoru.Name = "richTextBoxCsoru";
+            this.richTextBoxCsoru.Size = new System.Drawing.Size(197, 147);
+            this.richTextBoxCsoru.TabIndex = 33;
+            this.richTextBoxCsoru.Text = "";
+            // 
+            // buttonGeri
+            // 
+            this.buttonGeri.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonGeri.Location = new System.Drawing.Point(251, 214);
+            this.buttonGeri.Name = "buttonGeri";
+            this.buttonGeri.Size = new System.Drawing.Size(66, 23);
+            this.buttonGeri.TabIndex = 32;
+            this.buttonGeri.Text = "Geri";
+            this.buttonGeri.UseVisualStyleBackColor = true;
+            this.buttonGeri.Click += new System.EventHandler(this.buttonGeri_Click);
+            // 
+            // buttonileri
+            // 
+            this.buttonileri.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonileri.Location = new System.Drawing.Point(376, 214);
+            this.buttonileri.Name = "buttonileri";
+            this.buttonileri.Size = new System.Drawing.Size(66, 23);
+            this.buttonileri.TabIndex = 31;
+            this.buttonileri.Text = "İleri";
+            this.buttonileri.UseVisualStyleBackColor = true;
+            this.buttonileri.Click += new System.EventHandler(this.buttonileri_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBoxsorucevaplari);
+            this.groupBox2.Location = new System.Drawing.Point(251, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(191, 182);
+            this.groupBox2.TabIndex = 30;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cevaplar";
+            // 
+            // richTextBoxsorucevaplari
+            // 
+            this.richTextBoxsorucevaplari.Location = new System.Drawing.Point(5, 26);
+            this.richTextBoxsorucevaplari.Name = "richTextBoxsorucevaplari";
+            this.richTextBoxsorucevaplari.Size = new System.Drawing.Size(181, 148);
+            this.richTextBoxsorucevaplari.TabIndex = 8;
+            this.richTextBoxsorucevaplari.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.hakkinda});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(487, 24);
+            this.menuStrip1.TabIndex = 25;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kaydet,
+            this.cikis});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // kaydet
+            // 
+            this.kaydet.Name = "kaydet";
+            this.kaydet.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.kaydet.Size = new System.Drawing.Size(173, 22);
+            this.kaydet.Text = "Kaydet";
+            this.kaydet.Click += new System.EventHandler(this.kaydet_Click);
+            // 
+            // cikis
+            // 
+            this.cikis.Name = "cikis";
+            this.cikis.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.F4)));
+            this.cikis.Size = new System.Drawing.Size(173, 22);
+            this.cikis.Text = "Çıkış";
+            this.cikis.Click += new System.EventHandler(this.cikis_Click);
+            // 
+            // hakkinda
+            // 
+            this.hakkinda.Name = "hakkinda";
+            this.hakkinda.Size = new System.Drawing.Size(69, 20);
+            this.hakkinda.Text = "Hakkında";
+            this.hakkinda.Click += new System.EventHandler(this.hakkinda_Click);
+            // 
+            // SoruBankasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 320);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "SoruBankasi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soru Bankası";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.panelSorular.ResumeLayout(false);
+            this.panelSorular.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.Button btnTemizleCevaplar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSoruEkle;
+        private System.Windows.Forms.RichTextBox richTextBoxSoru;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panelSorular;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonGeri;
+        private System.Windows.Forms.Button buttonileri;
+        private System.Windows.Forms.RichTextBox richTextBoxCsoru;
+        private System.Windows.Forms.RichTextBox richTextBoxsorucevaplari;
+        private System.Windows.Forms.RichTextBox richTextBoxcevaplar;
+        private System.Windows.Forms.Button buttonSoruTemizle;
+        private System.Windows.Forms.Label labelSoru;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kaydet;
+        private System.Windows.Forms.ToolStripMenuItem cikis;
+        private System.Windows.Forms.ToolStripMenuItem hakkinda;
+    }
+}
+
---
+++ performans/SoruBankasi.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace performans
+{
+    public partial class SoruBankasi : Form
+    {
+        public SoruBankasi()
+        {
+            InitializeComponent();
+        }
+        
+        Dictionary<string, string> sorular = new Dictionary<string, string>();
+        int cevaplar = 0;
+        public void btnSoruEkle_Click(object sender, EventArgs e) // bu butona basıldığında dictinonary'e soru ve cevabı ekler.
+        {
+            cevaplar++;
+            try
+            {
+                string soru = richTextBoxSoru.Text;
+                string cevap = richTextBoxcevaplar.Text;
+                if (soru != "" && cevap != "")
+                {
+                    sorular.Add(soru, cevap);
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen soru ve cevapları doldurun.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Bir hata oluştu.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            richTextBoxSoru.Clear();
+            richTextBoxcevaplar.Text = "A) " + Environment.NewLine + "B) " + Environment.NewLine + "C) " + Environment.NewLine + "D) " + Environment.NewLine + "E) ";
+        }
+
+        int index = 0;
+        int soruNo = 1;
+        private void btnOlustur_Click(object sender, EventArgs e) // bu butona basıldığında soruları oluşturur ve sorular tabPage'ine geçer.
+        {
+            tabControl1.SelectTab(tabPage2);
+            panelSorular.Visible = true;
+            labelSoru.Text = soruNo + ". Soru";
+            foreach (var soru in sorular.Keys)
+            {
+                richTextBoxCsoru.Text = soru + Environment.NewLine;
+            }
+
+            foreach (var soru in sorular.Values)
+            {
+                richTextBoxsorucevaplari.Text = soru + Environment.NewLine;
+            }
+            
+        }
+       
+        private void buttonileri_Click(object sender, EventArgs e) // bu butona basıldığında bir sonraki soru ve cevaba geçer.
+        {
+            soruNo++;
+            labelSoru.Text = soruNo + ". Soru"; // 1. soru 2. soru şeklinde gözükmesi açısından label ile soruNo değişkenini birleştirir.
+            int say = sorular.Count;
+            if (index == say)
+            {
+                index = 0;
+            }
+
+            richTextBoxCsoru.Text = sorular.Keys.ElementAt(index); // Belirtilen indexteki soruyu richTextBox'a yazar. 
+            richTextBoxsorucevaplari.Text = sorular.Values.ElementAt(index); // Belirtilen indexteki cevabı richTextBox'a yazar.
+
+            index++;
+        }
+
+        private void buttonGeri_Click(object sender, EventArgs e) // bu butona basıldığında bir önceki soru ve cevaba geçer.
+        {
+            soruNo--;
+            labelSoru.Text = soruNo + ". Soru"; // 1. soru 2. soru şeklinde gözükmesi açısından label ile soruNo değişkenini birleştirir.
+            int say = sorular.Count;
+
+            if (index == 0)
+            {
+                index = say;
+            }
+
+            richTextBoxCsoru.Text = sorular.Keys.ElementAt(index - 1); // Belirtilen indexteki soruyu richTextBox'a yazar.
+            richTextBoxsorucevaplari.Text = sorular.Values.ElementAt(index - 1); // Belirtilen indexteki cevabı richTextBox'a yazar.
+
+            index--;
+        }
+
+        private void btnTemizleCevaplar_Click(object sender, EventArgs e)
+        {
+            richTextBoxcevaplar.Text = "A) " + Environment.NewLine + "B) " + Environment.NewLine + "C) " + Environment.NewLine + "D) " + Environment.NewLine + "E) ";
+        }
+
+        private void buttonSoruTemizle_Click(object sender, EventArgs e)
+        {
+            richTextBoxSoru.Clear();
+        }
+
+        private void cikis_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çıkmak istedğinizden emin misiniz?", "Dikkat", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            DialogResult sonuc = new DialogResult();
+            if (sonuc == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void kaydet_Click(object sender, EventArgs e) // Kaydet'e tıklanınca sorular dosya olarak kaydedilir.
+        {
+            int soruNoTxt = 1;
+            SaveFileDialog kaydet = new SaveFileDialog();
+            kaydet.Filter = "Metin Dosyası | *.txt | Word Dosyası | *.docx";
+            DialogResult sonuc = kaydet.ShowDialog();
+            if (sonuc == DialogResult.OK)
+            {
+                using (StreamWriter yazici = new StreamWriter(kaydet.FileName)) // RichTextBox'taki değerleri dosyaya yazmak için System.IO kütüphanesindeki StreamWriter'ı kullandım.
+                {
+                    foreach (var soru in sorular)
+                    {
+                        yazici.WriteLine(soruNoTxt + ". Soru");
+                        yazici.WriteLine(soru.Key);
+                        yazici.WriteLine(soru.Value);
+                        yazici.WriteLine(Environment.NewLine);
+                        soruNoTxt++;
+                    }
+                }
+            }
+        }
+
+        private void hakkinda_Click(object sender, EventArgs e)
+        {
+            Hakkinda hakkinda = new Hakkinda();
+            hakkinda.Show();
+        }
+    }
+}
